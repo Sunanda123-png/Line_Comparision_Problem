c7c7e623a887d17abf381506c9846d2cb10d0549
Sunanda123-png <sunanda02@gmail.com>
2021-12-21 14:42:32 +0530

    Add: calculate length test case

--- line_comparision_test.py
+++ line_comparision_test.py
@@ -2,5 +2,9 @@ from line_comparision import Line
 
 
 def test_calculate_length():
+    """
+    test case for calculate length method
+    :return:
+    """
     l1 = Line(2, 4, 2, 4)
     assert isinstance(l1, Line) == True
