85e2ec094538bc904d91803c4b407924c62a74ae
Sunanda123-png <sunanda02@gmail.com>
2021-12-24 23:35:34 +0530

    Add: test case for adding line

--- line_comparision_test.py
+++ line_comparision_test.py
@@ -1,4 +1,5 @@
 from line_comparision import Line
+from line_comparision import LineComparision
 
 
 def test_calculate_length():
@@ -6,5 +7,19 @@ def test_calculate_length():
     test case for calculate length method
     :return:
     """
-    l1 = Line(2, 4, 2, 4)
+    line_dict = {"name": "sunanda", "x1": 2, "x2": 4, "y1": 2, "y2": 4}
+    l1 = Line(line_dict)
     assert isinstance(l1, Line) == True
+
+
+def test_add_line():
+    """
+    test case for add line method
+    :return:
+    """
+    line_comparision = LineComparision()
+    line_dict = {"name": "sunanda", "x1": 2, "x2": 4, "y1": 2, "y2": 4}
+    l1 = Line(line_dict)
+    line_comparision.add_line(l1)
+    assert len(line_comparision.line_list) > 0
+
